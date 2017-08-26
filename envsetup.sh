322321e84d2619c9d5d9171a66a2d3bf7468a3c2
Guillaume Lesniak <me@xplod.fr>
2017-08-26 12:12:51 -0400

    Add brunch/breakfast aliases
    Change-Id: Ic4f15556a37fdf2e3a376c2f6dc9718c90a3a1a6
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -568,6 +568,52 @@ function print_lunch_menu()
     echo
 }
 
+function breakfast()
+{
+    target=$1
+    local variant=$2
+    CUSTOM_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    for f in `/bin/ls vendor/eucos/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the model name
+            if [ -z "$variant" ]; then
+                variant="user"
+            fi
+            lunch $target-$variant
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        time mka bacon
+    else
+        echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
+
 function lunch()
 {
     local answer
